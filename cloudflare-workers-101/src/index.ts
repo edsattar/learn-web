cadde62b9f155d90ac7ffaf5c23321d2b39e54d7
edsattar <ahmed.sattar@live.com>
2024-10-31 15:51:02 +0600

    add hono

--- cloudflare-workers-101/src/index.ts
+++ cloudflare-workers-101/src/index.ts
@@ -1,18 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export interface Bindings { }
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/", (ctx) => {
+	return ctx.json({ message: "Hello, World! Hello from Hono!" });
+});
+
+export default app;
