fa3956427087d824ca65c77dc368ef285c3810af
edsattar <ahmed.sattar@live.com>
2024-10-31 20:48:59 +0600

    add api endpoints

--- cloudflare-d1-http/src/index.ts
+++ cloudflare-d1-http/src/index.ts
@@ -1,18 +1,29 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+import { bearerAuth } from "hono/bearer-auth";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+  API_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("*", prettyJSON(), logger(), async (c, next) => {
+  const auth = bearerAuth({ token: c.env.API_KEY });
+  return auth(c, next);
+});
+
+app.post("/api/all", async (c) => {
+  return c.text("/api/all endpoint");
+});
+
+app.post("/api/exec", async (c) => {
+  return c.text("/api/exec endpoint");
+});
+
+app.post("/api/batch", async (c) => {
+  return c.text("/api/batch endpoint");
+});
+
+export default app;
